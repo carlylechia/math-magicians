35215d78a6d35215062f095d038a2409b3f2dd99
Chia Carlyle <chiacarlyle@gmail.com>
2022-04-08 07:56:55 +0100

    Make styling and UX adjustments as requested by code-reviewer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,9 +7,6 @@ const Home = () => (
     <p className="home-text pl4 f4 tl">
       Mathematics is generally defined as the science that deals with numbers. It involves operations among numbers, and it also helps you to calculate the product price, how many discounted prizes here, and If you good in maths so you can calculate very fast. Mathematicians and scientists rely on mathematics principles in their real-life to experiments with new things every day. Many students say that ” I hate mathematics ” and maths is a useless subject, but it is wrong because without mathematics your life is tough to survive. Math has its applications in every field.
     </p>
-    <p className="home-text two pl4 pr2 f4 tl">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    </p>
   </div>
 );
 
